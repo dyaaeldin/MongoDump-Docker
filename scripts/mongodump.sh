05bcc5f5f65fed057721db89b33d72fa9fe41984
Dyaa Ahmed <79382618+dyaaeldin@users.noreply.github.com>
2021-03-19 16:38:55 +0200

    Create mongodump.sh

---
+++ scripts/mongodump.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+for dir in tmp mnt; do
+  if [ ! -d "/$dir/$BACKUP_DIR" ]; then
+     mkdir /mnt/$BACKUP_DIR 
+  fi
+done  
+
+mongodump --host="$DB_SERVICE_NAME" --port="$DB_PORT" --db="$MONGODB_DATABASE" --username="$MONGODB_USERNAME" --password="$MONGODB_PASSWORD" --out="/tmp/$BACKUP_DIR"
+cd /tmp
+tar cvf mongodb-$(date +%d-%m-%y-%H-%M).tar $BACKUP_DIR
+cp mongodb-*.tar /mnt/$BACKUP_DIR
+if [ "$REMOTE_BACKUP" = "true" ];then
+scp -o StrictHostKeyChecking=no -P $REMOTE_SERVER_PORT -i $KEY_PATH /tmp/mongodb-*.tar $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+fi
+rm -rf mongodb-*.tar /tmp/$BACKUP_DIR
+find /mnt -mindepth 1 -mtime +$BACKUP_TIME -delete
